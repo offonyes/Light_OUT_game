508d76258e388dc701dc436cdd5e324bb4fdc44b
Dimitri_Kat <49370568+offonyes@users.noreply.github.com>
2023-02-19 20:14:42 +0400

    Update Turn_light.py

--- Turn_light.py
+++ Turn_light.py
@@ -5,19 +5,21 @@ from PIL import Image, ImageTk
 class Turn_Light(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
-        master.title("Light OUT(FIRST GUI)") #Controls the window title.
+        master.title("First GUI") #Controls the window title.
         master.geometry("350x350+200+200") #size
         self.pack()
         self.createWidgets()
         text = tk.Label(self, text="LIGHT OUT",fg="Black",font=('Helvetica bold', 26))
         text.place(x=1,y= 20)
         self.move = 0
-        self.Moves = tk.Label(self, text=f"Moves\n{self.move}",width=4,height=2)
-        self.Moves.grid(row = 6, column = 2 ,pady=(25,0))
+        self.Moves = tk.Button(self, text=f"Moves\n{self.move}",width=7,height=2,state="disabled")
+        self.Moves.grid(row = 2, column = 7 ,padx=(25,0))
         self.count = 0
+        master.resizable(False, False)
         icon_1 = Image.open("light_on\img.png")
         photo = ImageTk.PhotoImage(icon_1)
         root.wm_iconphoto(False, photo)
+
     def createWidgets(self):
         floors = [i for i in range(1,26)]
         self.buttons = {}
@@ -42,7 +44,9 @@ class Turn_Light(tk.Frame):
                 e.append([xPos,yPos])
                 yPos = yPos +1
         self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                    command=root.destroy,width=4,height=2).grid(row = 2, column = 7 ,padx=(25,0))
+                    command=root.destroy,width=7,height=2).grid(row = 4, column = 7 ,padx=(25,0))
+        self.restart = tk.Button(self, text="RESTART", fg="red",
+                    command=self.restart_program,width=7,height=2).grid(row =3, column = 7 ,padx=(25,0))
 
     def pressed(self, button):
         self.move +=1
@@ -82,12 +86,17 @@ class Turn_Light(tk.Frame):
                 self.count -=1
         if self.count == 25:
             self.game_over()
+
     def game_over(self):
         showinfo("You won", "Congratulations you passed the game!")
         for i in range(1,26):
             self.buttons[i]["state"] = "disabled"
 
-      
+    def restart_program(self):
+        self.createWidgets()
+        self.move = 0
+        self.Moves.config(text = f"Moves\n{self.move}")
+        self.count = 0
 if __name__ == "__main__":
     root = tk.Tk()
     app = Turn_Light(master=root)
